c3aea3526028bb72560909fd9bab67b0436e9687
Jay <jy4763@gmail.com>
2023-09-24 18:45:29 -0400

    Function for generating tags using machine learning model albumy/ml_utils.py

--- README.md
+++ README.md
@@ -42,3 +42,13 @@ Test account:
 
 This project is licensed under the MIT License (see the
 [LICENSE](LICENSE) file for details).
+
+Notes on Assignment 1 work:
+- Functions for updating description/tags seems to be in albumy/albumy/blueprints/main.py
+- Seems like in the upload() function of albumy/albumy/blueprints/main.py, we can change the description and the tags using the Photo class intiailization. This class is defined in albumy/albumy/utils.py
+
+Notes on preparing recognize anything model (model used for tagging and/or captioning):
+- https://github.com/xinyu1205/recognize-anything
+- Have to run: pip install git+https://github.com/xinyu1205/recognize-anything.git
+    - But requires rust to work, install using: curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+- Running the code to install the pretrained model weights requires wget to be installed
---
+++ albumy/ml_utils.py
@@ -0,0 +1,75 @@
+import os
+import subprocess
+import torch
+
+from PIL import Image
+from ram.models import ram
+from ram.models import tag2text
+from ram import inference_ram
+from ram import inference_tag2text
+from ram import get_transform
+
+# Based on https://github.com/xinyu1205/recognize-anything/blob/main/inference_tag2text.py
+
+
+def get_ml_tags(image_path, image_size=384, threshold=0.68, model_type='RAM'):
+    # Regarding image_size default: help message says 448 but code says 384
+    if model_type not in ['RAM', 'tag2text']:
+        raise ValueError('model_type argument must be RAM or tag2text')
+    
+    if model_type == 'RAM':
+        pretrained_path = 'ram_swin_large_14m.pth'
+    elif model_type == 'tag2text':
+        pretrained_path = 'tag2text_swin_14m.pth'
+
+    # If pretrained weights do not exist, download them:
+    cwd = os.getcwd()
+    pretrained_dir_path = os.path.join(cwd, 'pretrained')
+    if not os.path.exists(pretrained_dir_path):
+        os.mkdir(pretrained_dir_path)
+    pretrained_path = os.path.join(pretrained_dir_path, pretrained_path)
+    if not os.path.exists(pretrained_path):
+        if model_type == 'RAM':
+            install_link = 'https://huggingface.co/spaces/xinyu1205/Recognize_Anything-Tag2Text/resolve/main/ram_swin_large_14m.pth'
+        elif model_type == 'tag2text':
+            install_link = 'https://huggingface.co/spaces/xinyu1205/Recognize_Anything-Tag2Text/resolve/main/tag2text_swin_14m.pth'
+        process = subprocess.Popen('wget ' + install_link + ' -O ' + pretrained_path,
+                                   stdout=subprocess.PIPE,
+                                   stderr=subprocess.PIPE,
+                                   text=True,
+                                   shell=True)
+        exit_codes = process.wait()
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    transform = get_transform(image_size=image_size)
+
+    if model_type == 'RAM':
+        model = ram(pretrained=pretrained_path,
+                    image_size=image_size,
+                    vit='swin_l')
+    elif model_type == 'tag2text':
+        delete_tag_index = [127, 2961, 3351, 3265, 3338, 3355, 3359]
+        model = tag2text(pretrained=pretrained_path,
+                         image_size=image_size,
+                         vit='swin_b',
+                         delete_tag_index=delete_tag_index)
+        model.threshold = threshold
+
+    model.eval()
+    model = model.to(device)
+    image = transform(Image.open(image_path)).unsqueeze(0).to(device)
+    if model_type == 'RAM':
+        res = inference_ram(image, model)
+        # print("Image Tags: ", res[0])
+        # res[1] raises an error when trying to print
+    elif model_type == 'tag2text':
+        res = inference_tag2text(image, model, 'None') # Third argument is for specified tags
+        # print("Model Identified Tags: ", res[0])
+        # print("User Specified Tags: ", res[1])
+        # print("Image Caption: ", res[2])
+    tags = res[0].split(" | ") # Both model types have tags at 0th index
+    print("Tags: ", tags)
+
+if __name__ == '__main__':
+    test_image_path = 'C:/Users/Jay/Documents/phd/csc2125/CSC2125_2023/uploads/70ff8f4a3612431ab58d9509e19075d2.jpg'
+    get_ml_tags(test_image_path)
